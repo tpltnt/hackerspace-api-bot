48d573bedcb8e715ad313e3aee1739e3bbdb9c1c
tpltnt <tpltnt.github@dropcut.net>
2014-04-04 17:29:52 +0200

    basic bot code added

--- bot.py
+++ bot.py
@@ -3,3 +3,19 @@
 """
 Make your hackerspace a XMPP buddy.
 """
+
+import sleekxmpp
+
+
+class HackerspaceApiBot(sleekxmpp.ClientXMPP):
+    """
+    A SleekXMPP based bot that will mimic the on-/offline 
+    status of a given hackerspace.
+    """
+    def __init__(self, jid, password):
+        super(HackerspaceApiBot, self).__init__(jid, password)
+        self.add_event_handler('session_start', self.start)
+
+    def start(self, event):
+        self.send_presence("hello world")
+        self.get_roster()
